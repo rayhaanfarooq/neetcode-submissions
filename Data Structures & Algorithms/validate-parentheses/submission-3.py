997bd276a14b42ca00f9d0599594d866f466f378
Rayhaan Farooq <145854672+rayhaanfarooq@users.noreply.github.com>
2026-09-21 20:21:08 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,56 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        '''
+
+        So we have 2 different kinds of bracks 
+        (
+        [
+        {
+
+        So we can use 3 stacks one for each
+        just append the {
+        then when we get a } pop one from the stack. Then check
+        if all 3 are empty
+
+        We need a end condition, is it to check if the others are
+        empty?
+
+        One stack
+
+        if its a closing, check if the other one is a opening
+
+        Stack: [(
+
+
+        '''
+        stack = []
+
+        opening = {"(" : ")", "{" : "}", "[" : "]"}
+        closing = {")" : "(", "}" : "{", "]" : "["}
+        
+
+
+        for char in s:
+            if char in opening:
+                stack.append(char)
+
+            else:
+                if stack and stack[-1] == closing[char]:
+                    stack.pop()
+
+                else:
+                    return False
+
+        return stack == []
+
+
+
+
+
+
+
+
+
+        
+        
